58e583a2311cdf3eea3555d6a2057cd94f36c92c
Elif Ceren Ankara <ceren.ankara.e@gmail.com>
2026-01-27 12:59:56 +0300

    Create run_example.py
    Add example script for ethanol molecule

---
+++ examples/run_example.py
@@ -0,0 +1,15 @@
+from morgan_algorithm import morgan
+
+labels, canonical = morgan("ethanol.mol")
+
+print(" ")
+print("Molecule File: ethanol.mol")
+print("-" * 60)
+print("Atom Index - Morgan Label - Canonical Index")
+print("-" * 60)
+
+for i, value in enumerate(labels):
+    print(" " * 3, i + 1, " " * 12, value, " " * 12, canonical[i])
+
+print("-" * 60)
+print("Final List:", labels)
